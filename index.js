85b143de33e3d333325ffc76cbd0730053c8be9c
hitode909 <hitode909@gmail.com>
2020-05-02 10:21:52 +0900

    rewrite

--- index.js
+++ index.js
@@ -1,44 +1,56 @@
-exports.accept = (req, res) => {
+function doPost(req, res) {
+  const datastore = require('@google-cloud/datastore')();
+
+  const entityToSave = createEntityToSave(datastore, req);
+  datastore.save(entityToSave).then(() => {
+    res.status(200).send('OK');
+  }).catch(err => {
+    console.error(err);
+    res.status(500).send(err);
+  });
+}
+
+function createEntityToSave(datastore, req) {
+  const key = datastore.key('History');
+  const data = [];
+  req.body.timestamp = new Date();
+  Object.keys(req.body).forEach(key => {
+    data.push({
+      name: key,
+      value: req.body[key],
+    });
+  });
+  return {
+    key: key,
+    data: req.body,
+  };
+}
+
+function doGet(req, res) {
+  const datastore = require('@google-cloud/datastore')();
+
+  let q = datastore.createQuery('History').order('timestamp', { descending: true, });
+  const pwd = req.query.pwd;
+  if (pwd) {
+    q = q.filter('pwd', '=', pwd);
+  }
+  q.run(function (err, entities, info) {
+    console.log(err);
+    res.status(200).send(entities);
+  });
+}
+
+exports.accept = async (req, res) => {
   let apiKey = req.query.apikey;
   if (apiKey !== process.env.APIKEY) {
     res.status(401).send('apiKey required');
     return;
   }
 
-  const datastore = require('@google-cloud/datastore')();
-
   if (req.method === 'POST') {
-    const key = datastore.key('History');
-    const data = [];
-    req.body.timestamp = new Date();
-    Object.keys(req.body).forEach(key => {
-      data.push({
-        name: key,
-        value: req.body[key],
-      });
-    });
-    const entity = {
-      key: key,
-      data: req.body,
-    };
-    datastore.save(entity).then(() => {
-      res.status(200).send(key.id);
-    })
-      .catch(err => {
-        console.error(err);
-        res.status(500).send(err);
-      });
+    return doPost(req, res);
   }
   if (req.method === 'GET') {
-    req.param
-    let q = datastore.createQuery('History').order('timestamp', { descending: true, });
-    const pwd = req.query.pwd;
-    if (pwd) {
-      q = q.filter('pwd', '=', pwd);
-    }
-    q.run(function (err, entities, info) {
-      console.log(err);
-      res.status(200).send(entities);
-    });
+    doGet(req, res);
   }
 };
